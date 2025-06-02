b5973baf3972e326893eb4c6ed0b0e25102f44b0
isaiahsamson49 <isaiahsamson49@gmail.com>
2025-06-02 17:48:22 +1200

    Initial Commit

---
+++ Variable.py
@@ -0,0 +1,8 @@
+x=5
+y="John"
+codingal="codingal"
+name=input("Enter your name")
+print(x)
+print(y)
+print(codingal)
+print(name)
---
+++ print.py
@@ -0,0 +1,8 @@
+print('Codingal')
+print("Hello Welcome to Python n")
+print(8)
+print("Python",8)
+print("Welcome to the", end= "*")
+
+
+
